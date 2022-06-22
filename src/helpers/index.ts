3c26245d76a69f378c8831169d7ce811fe91cb41
essenija1 <80848058+essenija1@users.noreply.github.com>
2022-06-22 15:13:16 +0300

    Update index.ts

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,1 +1,28 @@
 
+import { renderSearchFormBlock, transferData } from './search-form.js'
+import { renderSearchStubBlock } from './search-results.js'
+import { renderUserBlock, getUserData, getFavoritesAmount } from './user.js'
+import { renderToast } from './lib.js'
+
+
+window.addEventListener('DOMContentLoaded', () => {
+
+  const personObj = getUserData('username');
+  const fav = getFavoritesAmount('favoritesAmount');
+
+  if (personObj == null) {
+    console.error('Нет пользователя');
+  } if (typeof (fav) == 'number') {
+    renderUserBlock(personObj.username, personObj.avatarUrl, fav);
+  }
+
+  renderSearchFormBlock('', '');
+  renderSearchStubBlock();
+  renderToast(
+    { text: 'Это пример уведомления. Используйте его при необходимости', type: 'success' },
+    { name: 'Понял', handler: () => { console.log('Уведомление закрыто') } }
+  )
+
+  transferData();
+
+})
